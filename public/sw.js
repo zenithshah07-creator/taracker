1e8f18fd520864d986c4f2ea2c82f39a755522fa
zenith Shah <zenithshah07@gmail.com>
2026-01-08 22:00:01 +0545

    Inital changes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'goaltracker-v1';
+const CACHE_NAME = 'goaltracker-v2-' + new Date().getTime();
 const urlsToCache = [
     '/',
     '/index.html',
@@ -6,9 +6,24 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('GoalTracker Pro v2.0 - Loaded', new Date());
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
